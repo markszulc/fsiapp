29e488c0fa826efc644908943c17d95a1ab39f3e
Mark Szulc <mszulc@adobe.com>
2021-02-25 21:42:29 +1100

    Initial Commit

---
+++ fsiapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fsiapp
+//
+//  Created by Mark Szulc on 25/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, GitHub!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fsiapp/fsiappApp.swift
@@ -0,0 +1,17 @@
+//
+//  fsiappApp.swift
+//  fsiapp
+//
+//  Created by Mark Szulc on 25/2/21.
+//
+
+import SwiftUI
+
+@main
+struct fsiappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
